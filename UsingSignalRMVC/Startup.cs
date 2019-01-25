b7a834f900c4e4da6f79e2ee693269ff6fb3c76d
cutkit <tungldinfo@gmail.com>
2019-01-25 15:56:20 +0700

    Update Startup.cs

--- UsingSignalRMVC/Startup.cs
+++ UsingSignalRMVC/Startup.cs
@@ -12,7 +12,10 @@ namespace UsingSignalRMVC
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+<<<<<<< HEAD
             app.MapSignalR();
+=======
+            app.MapSignalR();//aadd
         }
     }
 }
